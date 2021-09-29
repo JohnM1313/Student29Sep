db06b6e8efede18e5c1a5ea3ca753158be2f5362
JohnM1313 <millajoh@shernet.sheridancollege.ca>
2021-09-29 19:55:10 -0400

    Added address

--- src/studentlist/studentList.java
+++ src/studentlist/studentList.java
@@ -13,9 +13,9 @@ public class studentList {
     public static void main(String[] args){
         /*added comment on 7:21*/
         Student[] studentList = new Student[3];
-        studentList[0] = new Student("1","Ronak Sheth");
-        studentList[1] = new Student("2","John Doe");
-        studentList[2] = new Student("3","abc");
+        studentList[0] = new Student("1","Ronak Sheth","55 Younge Street");
+        studentList[1] = new Student("2","John Doe","2020 Tanners Drive");
+        studentList[2] = new Student("3","abc","222 Summer Lane");
         
         for(int  i = 0;i<studentList.length;i++){
             System.out.println(studentList[i].getStudentName());
